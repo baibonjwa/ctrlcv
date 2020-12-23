8d169915320c3cead1db9964375b72080d9f52ec
baibonjwa <baibonjwa@gmail.com>
2020-12-23 19:02:02 +0800

    Change breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx,md}"],
   darkMode: false, // or 'media' or 'class'
+  corePlugins: {
+    container: false
+  },
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '540px',
+          },
+          '@screen md': {
+            maxWidth: '720px',
+          },
+          '@screen lg': {
+            maxWidth: '960px',
+          },
+          '@screen xl': {
+            maxWidth: '1140px',
+          },
+        }
+      })
+    }
+  ],
 };
